7ab3ade270e69d51a96a812753e6ff58ca201513
Quan Ming Vang <mingvang2003@gmail.com>
2025-03-28 13:57:45 +0000

    Added a 16x16 grid with a red outline, script.js and styles.css updated

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use scrict";
+
+const container = document.querySelector("#container");
+
+// Creating 16x16 div grid
+for(let i = 0; i < 256; i++) {
+    let temp = document.createElement("div");
+    temp.classList.add("grid");
+    container.appendChild(temp);
+}
